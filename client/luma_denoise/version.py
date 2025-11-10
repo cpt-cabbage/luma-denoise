e79f23640e2a46ee9626be7512abc122195943de
Luma <luma@luma.co.za>
2025-11-10 16:44:28 +0200

    updated version to 0.0.1

--- client/luma_denoise/version.py
+++ client/luma_denoise/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring luma-denoise addon version."""
-__version__ = "0.0.1+dev"
+__version__ = "0.0.1"
--- package.py
+++ package.py
@@ -1,13 +1,13 @@
 name = "luma-denoise"
 title = "Luma Denoise"
-version = "0.0.1+dev"
+version = "0.0.1"
 
 client_dir = "luma_denoise"
 
-ayon_server_version = ">=1.10.3"
+ayon_server_version = ">=1.13.1"
 ayon_required_addons = {
-    "core": ">=1.4.1",
-    "applications": ">=1.1.4",
-    "deadline": ">=0.5.12",
+    "core": ">=1.6.7",
+    "applications": ">=1.2.4",
+    "deadline": ">=0.5.18",
 }
 ayon_compatible_addons = {}
