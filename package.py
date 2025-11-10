d56e3c28bdcf5b524674a7b42b0846e11fb1af48
Luma <luma@luma.co.za>
2025-11-10 16:54:46 +0200

    fixed dealine dep

--- package.py
+++ package.py
@@ -8,6 +8,6 @@ ayon_server_version = ">=1.13.1"
 ayon_required_addons = {
     "core": ">=1.6.7",
     "applications": ">=1.2.4",
-    "deadline": ">=0.5.18",
+    "deadline": ">=0.5.18-ls.0.1.0",
 }
 ayon_compatible_addons = {}
