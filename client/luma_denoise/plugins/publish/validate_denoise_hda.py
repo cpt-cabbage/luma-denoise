d55fce635d4d7bbc14bbe8129942abdf4266331d
Christophe Leyder <christophe.leyder@ls.int>
2025-11-11 12:55:37 +0200

    add validate denoise HDA plugin

---
+++ client/luma_denoise/plugins/publish/validate_denoise_hda.py
@@ -0,0 +1,58 @@
+import re
+import os
+
+import hou
+import pyblish.api
+
+from ayon_core.pipeline import AYONPyblishPluginMixin
+from ayon_houdini.api import plugin
+
+
+class ValidateDenoiseHda(plugin.HoudiniInstancePlugin, AYONPyblishPluginMixin):
+    """Validate and set denoise parameter on Karma Render HDA
+
+    Reads the "denoise" publish attribute and sets the rmdenoise_aovs parameter
+    on the Houdini render node accordingly.
+
+    """
+
+    label = "Validate Denoise HDA"
+    order = pyblish.api.ValidatorOrder 
+    families = ["usdrender"]
+    
+    def get_denoise_enabled(self, instance):
+        """Get the denoise setting from publish attributes"""
+        # The BoolDef "denoise" is defined in HoudiniSubmitDeadlineUsdRender plugin
+        # We need to access it from that plugin's namespace in publish_attributes
+        publish_attributes = instance.data.get("publish_attributes", {})
+        deadline_attrs = publish_attributes.get("HoudiniSubmitDeadlineUsdRender", {})
+        denoise = deadline_attrs.get("denoise", "HIII")  # Default to True as defined in the BoolDef
+        self.log.debug(f"Denoise setting from publish attributes: {denoise}")
+        return denoise
+
+    def process(self, instance):
+        context = instance.context
+
+        # Ensure all previous results are successful
+        assert all(
+            result["success"] for result in context.data["results"]
+        ), "Errors found, aborting integration.."
+
+        # Get denoise setting from publish attributes
+        denoise = self.get_denoise_enabled(instance)
+        instancename = instance.data["name"]
+
+        # Get the /stage network first
+        stage = hou.node('/stage')
+
+        # Then get the node within it
+        node = stage.node(instancename)
+
+        # Set the rmdenoise_aovs parameter on the render node
+        node.parm("rmdenoise_aovs").set(bool(denoise))
+
+        self.log.info(f"Setting rmdenoise_aovs on {instancename} to {denoise}")
+                
+                
+
+        
