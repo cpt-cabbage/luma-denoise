2860d48feba4967f72c2b9dd9c3c20ec9f5ee2a8
Christophe Leyder <christophe.leyder@ls.int>
2026-01-23 15:47:02 +0200

    updae version

--- client/luma_denoise/version.py
+++ client/luma_denoise/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring luma-denoise addon version."""
-__version__ = "0.0.3"
+__version__ = "0.0.7"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "luma-denoise"
 title = "Luma Denoise"
-version = "0.0.3"
+version = "0.0.7"
 
 client_dir = "luma_denoise"
 
